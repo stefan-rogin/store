baf917ee152c08bfb088816b636a2d6d4a6d0858
stefan-rogin <stefan.rogin@outlook.com>
2025-04-01 08:35:18 +0000

    security UserDetails from repository

--- src/main/java/com/example/store/security/repository/UserRepository.java
+++ src/main/java/com/example/store/security/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.store.security.repository;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.store.security.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,13 @@
 insert into role(name) values('User');
 insert into role(name) values('Administrator');
 
+insert into users(username,password) values('user', 'user');
+insert into users(username,password) values('admin', 'admin');
+
+insert into users_roles(roles_id, user_id) values(1, 1);
+insert into users_roles(roles_id, user_id) values(1, 2);
+insert into users_roles(roles_id, user_id) values(2, 2);
+
 insert into price(amount, currency, deleted) values(1.49, 'EUR', false);
 insert into price(amount, currency, deleted) values(2.49, 'EUR', false);
 insert into price(amount, currency, deleted) values(3.49, 'EUR', false);
