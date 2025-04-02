6c1dbe74f87a37b91917aa77fb6dc45c6ebba035
stefan-rogin <stefan.rogin@outlook.com>
2025-04-02 13:19:48 +0000

    cleanup

--- src/main/java/com/example/store/service/ProductService.java
+++ src/main/java/com/example/store/service/ProductService.java
@@ -1,8 +1,6 @@
 package com.example.store.service;
 
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
@@ -15,8 +13,6 @@ import com.example.store.util.ProductUpdater;
 
 @Service
 public class ProductService {
-
-    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
 
     @Autowired
     private ProductRepository productRepository;
